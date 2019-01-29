bfffe6b2809fe398c81bb0f6c9e0c1786f198412
rupam-87 <44251643+rupam-87@users.noreply.github.com>
2019-01-29 10:12:06 +0530

    Add files via upload

---
+++ voidg.java
@@ -0,0 +1,18 @@
+public class voidg
+{
+public static void main(String[] args)
+{
+int n=56;
+String s,m,k;
+m=new String("rupam");
+k=new String("rupam");
+
+if(m.equals(k))
+{
+System.out.println(m);
+}
+s=Integer.toString(n);
+System.out.println(s);
+
+}
+}
